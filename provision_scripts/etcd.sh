a365f97646336204801b1099d0c7d9ce4f32ad41
meganokeefe <megan037@gmail.com>
2016-11-03 11:44:56 -0400

    mac cmd fix

--- provision_scripts/etcd.sh
+++ provision_scripts/etcd.sh
@@ -4,10 +4,11 @@ BASH_PROFILE=/home/vagrant/.bash_profile
 echo "Installing etcd v3..."
 ETCD_VER=v3.0.13
 DOWNLOAD_URL=https://github.com/coreos/etcd/releases/download
-curl -L ${DOWNLOAD_URL}/${ETCD_VER}/etcd-${ETCD_VER}-darwin-amd64.zip -o /tmp/etcd-${ETCD_VER}-darwin-amd64.zip
-mkdir -p /tmp/test-etcd && unzip /tmp/etcd-${ETCD_VER}-darwin-amd64.zip -d /tmp && mv /tmp/etcd-${ETCD_VER}-darwin-amd64 /tmp/test-etcd
+curl -L ${DOWNLOAD_URL}/${ETCD_VER}/etcd-${ETCD_VER}-linux-amd64.tar.gz -o /tmp/etcd-${ETCD_VER}-linux-amd64.tar.gz
+mkdir -p /tmp/test-etcd && tar xzvf /tmp/etcd-${ETCD_VER}-linux-amd64.tar.gz -C /tmp/test-etcd --strip-components=1
 
-#place binaries in expected locations 
+
+#place binaries in expected locations
 sudo mv /tmp/test-etcd/etcd /bin
 sudo mv /tmp/test-etcd/etcdctl /bin
 
