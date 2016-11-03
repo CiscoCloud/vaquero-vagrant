39abad1378adc0aceb5bf1166a120c7a0946538e
stephenrlouie <stephen.r.louie@gmail.com>
2016-11-03 11:05:11 -0400

    proper govendor install and placed in path

--- provision_scripts/govendor.sh
+++ provision_scripts/govendor.sh
@@ -2,3 +2,7 @@ source .bash_profile
 
 #govendor dependency for vaquero
 go get -u github.com/kardianos/govendor
+
+go build github.com/kardianos/govendor
+
+cp $GOPATH/src/github.com/kardianos/govendor/govendor /go/bin
