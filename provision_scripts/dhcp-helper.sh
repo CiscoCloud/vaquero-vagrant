886fffef6346fc7d4017070b8d6ffbd1b455430a
stephenrlouie <stephen.r.louie@gmail.com>
2016-11-04 16:11:13 -0400

    Multiple servers and agents configured

--- provision_scripts/dhcp-helper.sh
+++ provision_scripts/dhcp-helper.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 apt-get install dhcp-helper
-sed -i 's/-b eth0/-s 10.10.11.9/' /etc/default/dhcp-helper
+sed -i 's/-b eth0/-s 10.10.11.5/' /etc/default/dhcp-helper
 service dhcp-helper restart
