6782c8505664c44da76722deefc4b2123ad8a2ea
stephenrlouie <stephen.r.louie@gmail.com>
2017-01-26 15:49:59 -0500

    go 1.7.1

--- provision_scripts/general.sh
+++ provision_scripts/general.sh
@@ -4,9 +4,9 @@ yum install -y git vim screen
 BASH_PROFILE=/home/vagrant/.bash_profile
 
 cd /
-wget https://storage.googleapis.com/golang/go1.6.3.linux-amd64.tar.gz
-tar xzvf go1.6.3.linux-amd64.tar.gz
-rm go1.6.3.linux-amd64.tar.gz
+wget https://storage.googleapis.com/golang/go1.7.1.linux-amd64.tar.gz
+tar xzvf go1.7.1.linux-amd64.tar.gz
+rm go1.7.1.linux-amd64.tar.gz
 
 echo "export PATH=$PATH:/go/bin" >> $BASH_PROFILE
 echo "export GOROOT=/go" >> $BASH_PROFILE
