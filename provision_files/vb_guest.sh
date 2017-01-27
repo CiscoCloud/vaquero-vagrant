ca22099b4301cd4266f872c76f679c99fd7a78cc
stephenrlouie <stephen.r.louie@gmail.com>
2017-01-27 13:25:36 -0500

    5.0.24 version

--- provision_files/vb_guest.sh
+++ provision_files/vb_guest.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-wget http://download.virtualbox.org/virtualbox/5.0.16/VBoxGuestAdditions_5.0.16.iso -P /tmp
+wget http://download.virtualbox.org/virtualbox/5.0.24/VBoxGuestAdditions_5.0.24.iso -P /tmp
 
-sudo mount -o loop /tmp/VBoxGuestAdditions_5.0.16.iso /mnt
+sudo mount -o loop /tmp/VBoxGuestAdditions_5.0.24.iso /mnt
 
 sudo sh -x /mnt/VBoxLinuxAdditions.run # --keep
 
