25af8edbc1ff3c892e8eb3bdc5a9917395b8785d
stephenrlouie <stephen.r.louie@gmail.com>
2017-02-16 11:27:11 -0500

    Adding assets to work with current master, no ready probe yet

--- kube/vs.sh
+++ kube/vs.sh
@@ -2,5 +2,5 @@
 
 kubectl create secret generic ssh-key --from-file=/vagrant/provision_files/secret/server.pem --from-file=/vagrant/provision_files/secret/server.key
 kubectl create configmap vs-config --from-file=/vagrant/kube/vs-config.yaml
-kubectl create -f /vagrant/kube/vsPod.yaml
+kubectl create -f /vagrant/kube/vsRc.yaml
 kubectl create -f /vagrant/kube/vsSvc.yaml
